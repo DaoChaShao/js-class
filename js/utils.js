f49fa0c6ce3f9d15ae11e8b8f0c7d54893776333
DaoChaShao <27882272@qq.com>
2025-03-16 14:01:03 +1300

    perf: inherit

--- js/utils.js
+++ js/utils.js
@@ -15,10 +15,40 @@ export function PersonFunc(name, gender, age) {
     this.age = age;
 }
 
+PersonFunc.prototype.greet = function () {
+    return "How are you doing!";
+}
+
+export function StudentFunc(name, gender, age, score) {
+    PersonFunc.call(this, name, gender, age);
+    this.score = score;
+}
+StudentFunc.prototype = Object.create(PersonFunc.prototype);
+StudentFunc.prototype.constructor = StudentFunc;
+StudentFunc.prototype.greet = function () {
+    return "Hello, teacher!";
+}
+
+// class
 export class PersonClass {
     constructor(name, gender, age) {
         this.name = name;
         this.age = age;
         this.gender = gender;
+    }
+
+    greet() {
+        return "Hello, everyone!"
+    }
+}
+
+export class StudentClass extends PersonClass {
+    constructor(name, gender, age, score) {
+        super(name, gender, age);
+        this.score = score;
+    }
+
+    greet() {
+        return "Hello, teacher!";
     }
 }
